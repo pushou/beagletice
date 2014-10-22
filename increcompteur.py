3126c62b50bfc567e3ba233ef73ef54d57acf7cd
pushou <root@beaglebone-3.(none)>
2014-10-22 23:31:29 +0000

    gestion propre des CTRL C

--- increcompteur.py
+++ increcompteur.py
@@ -41,41 +41,54 @@ eteintLed()
 
 print('compteur: {} etat: {}'.format(compteur,etat))
 
-while True:
-    new_switch_state_12 = GPIO.input("P8_12")
-    new_switch_state_13 = GPIO.input("P8_13")
 
-    if new_switch_state_12 == 1 and old_switch_state_12 == 0 :
-        compteur+=1
-	#print('boutton P8_12 presse. Compteur:{} etat:{}'.format(compteur,etat))
-	if  GPIO.input("P8_11") == 0:
-            GPIO.output("P8_11",GPIO.HIGH)
-            GPIO.output("P8_10",GPIO.LOW)
-	    etat=1
-	elif GPIO.input("P8_11") == 1:
-            GPIO.output("P8_11",GPIO.LOW)
-	    etat=0
-	print('etat: {} , compteur: {}'.format(etat,compteur))
-	with  open('compteur.pkl', 'wb') as pkl_file:
-		pickle.dump({"compteur":compteur,"etat":etat},pkl_file)
-    old_switch_state_12 = new_switch_state_12
-     
-     
-    if new_switch_state_13 == 1 and old_switch_state_13 == 0 :
-        compteur+=1
-	if GPIO.input("P8_10") == 0:
-	    etat=2
+try:
+    while True:
+        new_switch_state_12 = GPIO.input("P8_12")
+        new_switch_state_13 = GPIO.input("P8_13")
+    
+        if new_switch_state_12 == 1 and old_switch_state_12 == 0 :
             compteur+=1
-            GPIO.output("P8_10",GPIO.HIGH)
-            GPIO.output("P8_11",GPIO.LOW)
-	elif GPIO.input("P8_10") == 1:
-            GPIO.output("P8_10",GPIO.LOW)
-	    etat=0
-	print('etat: {} , compteur: {}'.format(etat,compteur))
-	with  open('compteur.pkl', 'wb') as pkl_file:
-	      pickle.dump({"compteur":compteur,"etat":etat},pkl_file)
-    old_switch_state_13 = new_switch_state_13
+    	    if  GPIO.input("P8_11") == 0:
+                GPIO.output("P8_11",GPIO.HIGH)
+                GPIO.output("P8_10",GPIO.LOW)
+    	        etat=1
+    	    elif GPIO.input("P8_11") == 1:
+                GPIO.output("P8_11",GPIO.LOW)
+    	        etat=0
+    	    print('etat: {} , compteur: {}'.format(etat,compteur))
+    	    with  open('compteur.pkl', 'wb') as pkl_file:
+    		pickle.dump({"compteur":compteur,"etat":etat},pkl_file)
+        old_switch_state_12 = new_switch_state_12
+         
+         
+        if new_switch_state_13 == 1 and old_switch_state_13 == 0 :
+            compteur+=1
+    	    if GPIO.input("P8_10") == 0:
+    	        etat=2
+                compteur+=1
+                GPIO.output("P8_10",GPIO.HIGH)
+                GPIO.output("P8_11",GPIO.LOW)
+    	    elif GPIO.input("P8_10") == 1:
+                GPIO.output("P8_10",GPIO.LOW)
+    	        etat=0
+    	    print('etat: {} , compteur: {}'.format(etat,compteur))
+    	    with  open('compteur.pkl', 'wb') as pkl_file:
+    	        pickle.dump({"compteur":compteur,"etat":etat},pkl_file)
+        old_switch_state_13 = new_switch_state_13
+         
+        time.sleep(0.1)
      
-    time.sleep(0.1)
-     
-     
+except KeyboardInterrupt:
+    # here you put any code you want to run before the program 
+    # exits when you press CTRL+C
+    print("interruption volontaire") 
+except:
+    # this catches ALL other exceptions including errors.
+    # You won't get any error messages for debugging
+    # so only use it once your code is working
+    print("crash") 
+
+finally:
+    GPIO.cleanup() 
+
